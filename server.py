503de30cd13e3ec936c410376c7aebd625553f08
daniel <daniel.inama02@gmail.com>
2025-01-28 22:37:03 +0100

    add ftp

--- server.py
+++ server.py
@@ -1,7 +1,12 @@
 from http.server import HTTPServer, SimpleHTTPRequestHandler
 import os
+from pyftpdlib.handlers import FTPHandler
+from pyftpdlib.servers import FTPServer
+from pyftpdlib.authorizers import DummyAuthorizer
+import threading
 
 PORT = int(os.environ.get('PORT', 8080))
+FTP_PORT = int(os.environ.get('FTP_PORT', 2121))
 
 class VimrcHandler(SimpleHTTPRequestHandler):
     def do_GET(self):
@@ -14,7 +19,24 @@ class VimrcHandler(SimpleHTTPRequestHandler):
         else:
             self.send_error(404)
 
+def start_ftp_server():
+    authorizer = DummyAuthorizer()
+    authorizer.add_anonymous(os.getcwd(), perm='elradfmw')
+    
+    handler = FTPHandler
+    handler.authorizer = authorizer
+    
+    server = FTPServer(('', FTP_PORT), handler)
+    print(f"FTP server started at port {FTP_PORT}")
+    server.serve_forever()
+
 if __name__ == '__main__':
+    # Start FTP server in a separate thread
+    ftp_thread = threading.Thread(target=start_ftp_server)
+    ftp_thread.daemon = True
+    ftp_thread.start()
+    
+    # Start HTTP server
     with HTTPServer(("", PORT), VimrcHandler) as httpd:
-        print(f"Serving at port {PORT}")
+        print(f"HTTP server serving at port {PORT}")
         httpd.serve_forever()
